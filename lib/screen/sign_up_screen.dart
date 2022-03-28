abf635909caa6dcb1d8a89eba507989379cc4fa6
Triyono <yono.tri@gmail.com>
2022-03-28 21:04:38 +0700

    add signup

---
+++ lib/screen/sign_up_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController fullNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: fullNameController,
+            decoration: InputDecoration(
+              label: Text('Fullname'),
+              prefixIcon: Icon(Icons.person),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            controller: emailController,
+            decoration: InputDecoration(
+              label: Text('Email'),
+              prefixIcon: Icon(Icons.email),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            controller: passwordController,
+            decoration: InputDecoration(
+              label: Text('Password'),
+              prefixIcon: Icon(Icons.lock),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Signup')),
+        ],
+      ),
+    );
+  }
+}
